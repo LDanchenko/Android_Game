ea4b5a11290cc97633c4133decb69e503f17d4c1
LDanchenko <2l-u-b-o-v@ukr.net>
2017-02-08 15:40:49 +0200

    добавление магазина

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,12 +4,31 @@ using UnityEngine.UI;
 
 public class Game : MonoBehaviour {
 
-private int score = 0;
+private int bonus = 1;
+private int score;
 public Text scoreText;
+public GameObject shopPanel;
+
+	public void showPan_ShowAndHide(){
+	
+	shopPanel.SetActive(!shopPanel.activeSelf);
+		
+	}
+
+	public void shopButtonAddBonus(int bonus_plus){
+
+		if (score >=20) {
+			bonus += bonus_plus;
+	}
+
+	else {
+		Debug.Log("No money!");
+	}
+	}
 
 	public void OnClick (){
 
-	score += 1;
+	score += bonus;
  	scoreText.text = "SCORE = " + score;
 
 	}

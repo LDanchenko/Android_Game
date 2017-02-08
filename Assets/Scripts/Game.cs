ef825213478e559a8620c71cbdf37a9c48ef8905
LDanchenko <2l-u-b-o-v@ukr.net>
2017-02-08 09:42:52 +0200

    Создана кнопка для кликов + вывод score

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Game : MonoBehaviour {
+
+private int score = 0;
+public Text scoreText;
+
+	public void OnClick (){
+
+	score += 1;
+ 	scoreText.text = "SCORE = " + score;
+
+	}
+
+}

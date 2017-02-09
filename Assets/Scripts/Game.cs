0234ae2810444ee8a42c995777e2800efc126edd
LDanchenko <2l-u-b-o-v@ukr.net>
2017-02-09 15:33:51 +0200

    улучшения - бонусы

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 
 public class Game : MonoBehaviour {
@@ -7,7 +8,19 @@ public class Game : MonoBehaviour {
 private int bonus = 1;
 private int score;
 public Text scoreText;
+private int workersCount;
+[Header("Магазин")] //в инспекторе будет отображатся под надписью "Магазин"
+
 public GameObject shopPanel;
+public Text[] ShopBttnText;
+public int[] shopCosts;
+public int[] shopBonus;
+
+	public void Start()
+	{	//при запуске игры
+				StartCoroutine(BonusPerSec()); //метод для запуска нумератора (корутины)
+
+	}
 
 	public void showPan_ShowAndHide(){
 	
@@ -15,10 +28,13 @@ public GameObject shopPanel;
 		
 	}
 
-	public void shopButtonAddBonus(int bonus_plus){
+	public void shopButtonAddBonus(int index){
 
-		if (score >=20) {
-			bonus += bonus_plus;
+		if (score >= shopCosts[index]) {
+			bonus += shopBonus[index];
+			score -= shopCosts[index];
+			shopCosts[index] *= 2;
+			ShopBttnText[index].text = "КУПИТЬ УЛУЧШЕНИЕ\n" + shopCosts[index] + "$";
 	}
 
 	else {
@@ -29,8 +45,31 @@ public GameObject shopPanel;
 	public void OnClick (){
 
 	score += bonus;
- 	scoreText.text = "SCORE = " + score;
 
 	}
+
+	private void Update(){ //выполняется каждый кадр
+		scoreText.text = "SCORE = " + score + "$";
+	}
+
+	
+	public void HireWorker (int index)
+	{
+		if (score >= shopCosts[index])
+		{
+			workersCount ++;
+			score -= shopCosts[index];
+		}
+	}
+
+	IEnumerator BonusPerSec() //чтобы купить одного рабочего и получать +1 доллар каждую секунду
+		{
+			while  (true)
+			{
+				score += workersCount * 1;
+				yield return new WaitForSeconds(1);	
+			}
+			
+		}
 
 }
